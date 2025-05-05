4c6112c83b5e81b3fee651f309957dcc7416c692
JulianGibelli <juligibelli@gmail.com>
2025-05-05 16:13:16 -0300

    [FEATURE] added properties to set up ollama and pgvector

---
+++ src/test/java/com/example/rag_ollama/RagOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rag_ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
